ca3c60ea8df6c81f0eca159e232f14887c01da2f
Lenka Vraná <lenka.vrana@datlowe.cz>
2018-03-18 18:06:26 +0100

    Issue #3: library installable via pip

--- samples/CI_minimalPipeline.py
+++ samples/CI_minimalPipeline.py
@@ -4,7 +4,7 @@
 import os
 os.chdir('C:/path/') # Set path to CIF folder
 
-from CI_functions import *
+from cif import cif
 import pandas as pd
 import re
 import datetime
@@ -28,8 +28,8 @@ os.makedirs(outputDir, exist_ok = True)
 
 # 1) DATA LOAD (loading data from OECD API)
 
-data_all, subjects_all, measures_all = createDataFrameFromOECD(countries = [country], dsname = 'MEI', frequency = 'M')
-data_rs, subjects_rs, measures_rs = createDataFrameFromOECD(countries = [country], dsname = 'QNA', subject = ['B1_GE'], frequency = 'Q')
+data_all, subjects_all, measures_all = cif.createDataFrameFromOECD(countries = [country], dsname = 'MEI', frequency = 'M')
+data_rs, subjects_rs, measures_rs = cif.createDataFrameFromOECD(countries = [country], dsname = 'QNA', subject = ['B1_GE'], frequency = 'Q')
 
 
 # 1a) leading indicators: Component series
@@ -81,11 +81,11 @@ rsPriorityList = [ 'LNBQRSA' # Best fit with OECD reference series
 
 if (data_rs.shape[0] > 0):
     
-    rsq = getOnlyBestMeasure(df = data_rs, priorityList = rsPriorityList)
-    rsq = getRidOfMultiindex(df = rsq)
-    rsq = renameQuarterlyIndex(df = rsq)
-    rsq = getIndexAsDate(df = rsq)
-    rs = createMonthlySeries(df = rsq)
+    rsq = cif.getOnlyBestMeasure(df = data_rs, priorityList = rsPriorityList)
+    rsq = cif.getRidOfMultiindex(df = rsq)
+    rsq = cif.renameQuarterlyIndex(df = rsq)
+    rsq = cif.getIndexAsDate(df = rsq)
+    rs = cif.createMonthlySeries(df = rsq)
     rs.dropna(inplace = True)
 
 
@@ -94,7 +94,7 @@ if (data_rs.shape[0] > 0):
 #   & Normalisation
 
 fileLogs = open(os.path.join(outputDir, country + '_fileLogs_rsTransformation.txt'), 'w')
-rs_SA_HP_norm = pipelineTransformations(rs, savePlots = outputDir, saveLogs = fileLogs)
+rs_SA_HP_norm = cif.pipelineTransformations(rs, savePlots = outputDir, saveLogs = fileLogs)
 fileLogs.close()
 
 
@@ -122,9 +122,9 @@ priorityList = ['NCML'
 
 if data_adj.shape[0] > 0:
     
-    data = getOnlyBestMeasure(df = data_adj, priorityList = priorityList)
-    data = getRidOfMultiindex(df = data)
-    data = getIndexAsDate(data)
+    data = cif.getOnlyBestMeasure(df = data_adj, priorityList = priorityList)
+    data = cif.getRidOfMultiindex(df = data)
+    data = cif.getIndexAsDate(data)
 
 
 # 2.2b) Seasonal adjustment, outlier filtering and short-term prediction
@@ -132,7 +132,7 @@ if data_adj.shape[0] > 0:
 #   & Normalisation
 
 fileLogs = open(os.path.join(outputDir, 'fileLogs_dataTransformation.txt'), 'w')
-data_SA_HP_norm = pipelineTransformations(df = data, savePlots = outputDir, saveLogs = fileLogs, createInverse = True) 
+data_SA_HP_norm = cif.pipelineTransformations(df = data, savePlots = outputDir, saveLogs = fileLogs, createInverse = True) 
 fileLogs.close()
 
 
@@ -141,25 +141,25 @@ fileLogs.close()
 # 3.1) REFERENCE SERIES
 
 fileLogs = open(os.path.join(outputDir, country + '_fileLogs_rsEvaluation.txt'), 'w')
-rs_ind_turningPoints = pipelineTPDetection(rs_SA_HP_norm, savePlots = outputDir, saveLogs = fileLogs)
+rs_ind_turningPoints = cif.pipelineTPDetection(rs_SA_HP_norm, savePlots = outputDir, saveLogs = fileLogs)
 fileLogs.close()
     
 
 # 3.2) INDIVIDUAL INDICATORS
 
 fileLogs = open(os.path.join(outputDir, 'fileLogs_dataEvaluation.txt'), 'w')
-data_ind_turningPoints = pipelineTPDetection(df = data_SA_HP_norm, origColumns = list(data.columns), savePlots = outputDir, saveLogs = fileLogs)
+data_ind_turningPoints = cif.pipelineTPDetection(df = data_SA_HP_norm, origColumns = list(data.columns), savePlots = outputDir, saveLogs = fileLogs)
 fileLogs.close()
 
 
 # 4) TURNING-POINTS MATCHING
 
-data_ind_extOrd, data_ind_time, data_ind_missing, data_ind_missingEarly, data_ind_extra = pipelineTPMatching(df1 = rs_SA_HP_norm, df2 = data_SA_HP_norm, ind1 = rs_ind_turningPoints, ind2 = data_ind_turningPoints, savePlots = outputDir, nameSuffix = '_06_matching' + '_rs' + country)
+data_ind_extOrd, data_ind_time, data_ind_missing, data_ind_missingEarly, data_ind_extra = cif.pipelineTPMatching(df1 = rs_SA_HP_norm, df2 = data_SA_HP_norm, ind1 = rs_ind_turningPoints, ind2 = data_ind_turningPoints, savePlots = outputDir, nameSuffix = '_06_matching' + '_rs' + country)
 
 
 # 5) EVALUATION
 
-data_totalEval, data_selectedEval, data_selectedCol = pipelineEvaluation(df1 = rs_SA_HP_norm, df2 = data_SA_HP_norm, missing = data_ind_missing, missingEarly = data_ind_missingEarly, extra = data_ind_extra, time = data_ind_time, maxInd = 15)
+data_totalEval, data_selectedEval, data_selectedCol = cif.pipelineEvaluation(df1 = rs_SA_HP_norm, df2 = data_SA_HP_norm, missing = data_ind_missing, missingEarly = data_ind_missingEarly, extra = data_ind_extra, time = data_ind_time, maxInd = 15)
 
 
 # 6) AGGREGATION & FINAL EVALUATION 
@@ -168,23 +168,23 @@ data_totalEval, data_selectedEval, data_selectedCol = pipelineEvaluation(df1 = r
 
 agg_cMat = data_SA_HP_norm.loc[:, data_selectedCol] # value of the de-trended, smoothed and normalised component
 
-CLI = pipelineCreateCLI(agg_cMat).rename(columns = {'CLI': country + '_CLI'})
+CLI = cif.pipelineCreateCLI(agg_cMat).rename(columns = {'CLI': country + '_CLI'})
 
-compareTwoSeries(CLI, rs_SA_HP_norm)
+cif.compareTwoSeries(CLI, rs_SA_HP_norm)
 
 
 # 6b) CLI turning points
 
 fileLogs = open(os.path.join(outputDir, country + '_fileLogs_CLIEvaluation.txt'), 'w')
-CLI_ind_turningPoints = pipelineTPDetection(CLI, savePlots = outputDir, saveLogs = fileLogs)
+CLI_ind_turningPoints = cif.pipelineTPDetection(CLI, savePlots = outputDir, saveLogs = fileLogs)
 fileLogs.close()
 
 
 # 6c) Match turning points
 
-CLI_ind_extOrd, CLI_ind_time, CLI_ind_missing, CLI_ind_missingEarly, CLI_ind_extra = pipelineTPMatching(df1 = rs_SA_HP_norm, df2 = CLI, ind1 = rs_ind_turningPoints, ind2 = CLI_ind_turningPoints, savePlots = outputDir, nameSuffix = '_06_matching' + '_rs' + country, bw = bw)
+CLI_ind_extOrd, CLI_ind_time, CLI_ind_missing, CLI_ind_missingEarly, CLI_ind_extra = cif.pipelineTPMatching(df1 = rs_SA_HP_norm, df2 = CLI, ind1 = rs_ind_turningPoints, ind2 = CLI_ind_turningPoints, savePlots = outputDir, nameSuffix = '_06_matching' + '_rs' + country, bw = bw)
 
 
 # 6d) Basic characteristics
     
-CLI_eval = pipelineEvaluation(df1 = rs_SA_HP_norm, df2 = CLI, missing = CLI_ind_missing, missingEarly = CLI_ind_missingEarly, extra = CLI_ind_extra, time = CLI_ind_time, evalOnly = True)
+CLI_eval = cif.pipelineEvaluation(df1 = rs_SA_HP_norm, df2 = CLI, missing = CLI_ind_missing, missingEarly = CLI_ind_missingEarly, extra = CLI_ind_extra, time = CLI_ind_time, evalOnly = True)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cif',
+    version='0.0.1',
+    description='Composite Indicators Framework for Business Cycle Analysis',
+    license='gpl-3.0',
+	packages=find_packages(),
+	python_requires='>=3, <4',
+    author='Lenka Vraná',
+    author_email='lenka.vrana@gmail.com',
+    keywords=['business cycle analysis', 'composite indicators'],
+    url='https://github.com/LenkaV/CIF'
+)
