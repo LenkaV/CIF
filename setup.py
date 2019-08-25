e6e26ef34575a4ec5e8b612a1449cb2984b66141
Lenka Vraná <lenka.vrana@datlowe.cz>
2019-08-25 17:08:26 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='cif',
     version='0.0.3',
     description='Composite Indicators Framework for Business Cycle Analysis',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
     license='gpl-3.0',
-	packages=find_packages(),
+	packages=find_packages(where=".\cif"),
+	install_requires=['os', 'requests', 'pandas', 'numpy', 're', 'matplotlib', 'statsmodels', 'dateutil', 'warnings', 'zipfile'],
 	python_requires='>=3, <4',
     author='Lenka Vraná',
     author_email='lenka.vrana@gmail.com',
