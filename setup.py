acb8d47df33ac639df200ae9365c69dbdeacbb4f
Lenka Vraná <lenka.vrana@datlowe.cz>
2019-08-25 17:30:45 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages, find_namespace_packages
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
