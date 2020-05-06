4d55a2ef37f92fe06a7a5cc57d926a49b978bf00
LenkaV <lenka.vrana@gmail.com>
2020-05-06 09:42:13 +0200

    feat(cif): release version 0.0.6

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cif',
-    version='0.0.5',
+    version='0.0.6',
     description='Composite Indicators Framework for Business Cycle Analysis',
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='gpl-3.0',
     packages=find_packages(),
-    install_requires=['requests', 'pandas', 'matplotlib', 'statsmodels'],
+    install_requires=['requests', 'pandas>=0.24.0', 'matplotlib', 'statsmodels>=0.11.0'],
     python_requires='>=3, <4',
     author='Lenka Vraná',
     author_email='lenka.vrana@gmail.com',
-    keywords=['business cycle analysis', 'composite indicators'],
+    keywords=['business cycle analysis', 'composite indicators', 'OECD data download'],
     url='https://github.com/LenkaV/CIF'
 )
